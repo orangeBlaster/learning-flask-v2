3bd15a8becb94b26730b7350cd0731711cdb3767
orangeBlaster <148361840+orangeBlaster@users.noreply.github.com>
2023-10-21 00:25:12 +0000

    added connection to db

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs"))
+
+    jobs = []
+    result_all = result.all()
+    for row in result_all:
+      jobs.append(dict(row._mapping))
+    return jobs
